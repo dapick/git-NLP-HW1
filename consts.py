4e0968dac7cfec0559ed70db55f63ca7127bd12b
Daniella Pick <danielapick@gmail.com>
2017-12-08 19:03:53 +0200

    Updated tags as the tags in the train.wtag and added the paths to the data files

--- consts.py
+++ consts.py
@@ -1,5 +1,4 @@
 class Consts:
-    # TODO: put here only the tags we encounter in the training
     POS_TAGS = {'VBN', 'PRP$', 'PRP', '-LRB-', 'WP', 'PDT', 'VBP', ',', 'NNPS', 'WDT', 'NN', 'EX', '``', 'VBG', 'RP',
                 'TO', 'VBZ', 'VBD', 'JJ', 'NNP', 'FW', 'NNS', 'MD', 'CC', 'DT', '$', 'WP$', 'POS', '.', 'VB', 'RB',
                 'SYM', ':', 'CD', '-RRB-', 'JJR', 'RBS', 'WRB', '#', 'JJS', 'UH', 'RBR', "''", 'IN'}
