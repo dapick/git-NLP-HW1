4353e5695f04e17a2a623c390f07a019b2591d65
dblaus <dblaus@users.noreply.github.com>
2017-12-12 10:46:12 +0200

    back yo list from parser

--- tests/basicModelTest.py
+++ tests/basicModelTest.py
@@ -2,13 +2,17 @@ import unittest
 
 from consts import Consts
 from basicModel import BasicModel
+from fileTagger import Tagger
 
 
 class MyTestCase(unittest.TestCase):
     def test_v_parameter_calculation(self):
-        basic_model = BasicModel("../" + Consts.PATH_TO_TRAINING)
+        # basic_model = BasicModel("../" + Consts.PATH_TO_TRAINING)
+        basic_model = BasicModel("trialDataFiles/trialOneSentence.wtag")
         basic_model.calculate_v_parameter()
         print("The final v is:", basic_model.v_parameter)
+        tagger = Tagger("trialDataFiles/trialOneSentence.words")
+        tagger.tag_file()
 
 
 if __name__ == '__main__':
