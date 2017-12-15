339a393fbc780bf6c8c6b5d311df5feb41879e95
Daniella Pick <danielapick@gmail.com>
2017-12-15 10:58:02 +0200

    Updated the test to match the new constructor of 'Feature'

--- tests/featuresTest.py
+++ tests/featuresTest.py
@@ -6,12 +6,12 @@ from consts import Consts
 
 class FeaturesTestCase(unittest.TestCase):
     def test_count_features_types(self):
-        feature = Feature(Consts.TRAIN, "../" + Consts.PATH_TO_TRAINING)
+        feature = Feature(Consts.TRAIN, Consts.BASIC_MODEL, ["100", "103", "104"], "../" + Consts.PATH_TO_TRAINING)
         feature.count_features_types()
 
     def test_save_internal_fields(self):
-        feature = Feature(Consts.TRAIN, ["100", "103", "104"], "../" + Consts.PATH_TO_TRAINING)
-        new_feature = Feature(Consts.TAG)
+        feature = Feature(Consts.TRAIN, Consts.BASIC_MODEL, ["100", "103", "104"], "../" + Consts.PATH_TO_TRAINING)
+        new_feature = Feature(Consts.TAG, Consts.BASIC_MODEL)
         self.assertEqual(feature.feature_vector.items(), new_feature.feature_vector.items())
         self.assertEqual(feature.used_features, new_feature.used_features)
 
