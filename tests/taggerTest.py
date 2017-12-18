59d34d5999caa294d24d79cf1132a8c6ba06b3b5
dblaus <dblaus@users.noreply.github.com>
2017-12-18 02:43:22 +0200

    files for testing

--- tests/taggerTest.py
+++ tests/taggerTest.py
@@ -5,13 +5,14 @@ from tagger import Tagger
 
 class TaggerTestCase(unittest.TestCase):
     def test_short_sentence(self):
-        file_tagger = Tagger("trialDataFiles/short.words")
+        file_tagger = Tagger("trialDataFiles/test25Sentences.words")
         file_tagger.tag()
+        file_tagger.calculate_accuracy(file_tagger.tagged_file, "trialDataFiles/test25Sentences.exep")
 
     def test_accuracy(self):
         file_tagger = Tagger("trialDataFiles/short.words")
         # file_tagger.tag()
-        file_tagger.calculate_accuracy("trialDataFiles/expectedShort.wtag", file_tagger.tagged_file)
+        # file_tagger.calculate_accuracy("trialDataFiles/expectedShort.wtag", file_tagger.tagged_file)
 
 
 if __name__ == '__main__':
